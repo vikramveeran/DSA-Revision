da1d262a6ed8114ba80d750cc0abdd564f685633
vikramveeran <vikram.shanthipura@gmail.com>
2026-08-01 22:05:51 +0530

    Add files via upload

---
+++ LinkedList/DoublyDeletionHead.java
@@ -0,0 +1,47 @@
+class Node{
+    int data;
+    Node next;
+    Node back;
+
+    Node(int data1,Node next1,Node back1){
+        this.data = data1;
+        this.next = next1;
+        this.back = back1;
+    }
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+        this.back = null;
+    }
+}
+public class DoublyDeletionHead {
+     static Node convertArrToDLL(int arr[]){
+         Node head = new Node(arr[0]);
+         Node prev = head;
+         for(int i=1; i<arr.length; i++){
+             Node temp  = new Node(arr[i],null,prev);
+             prev.next = temp;
+             prev = temp;
+         }
+         return head;
+   }
+   static Node deletionHead(Node head){
+        Node prev = head;
+        head = head.next;
+        head.back = null;
+        prev.next = null;
+        return head;
+   }
+   static void print(Node head){
+      while(head != null){
+         System.out.println(head.data);
+         head = head.next;
+      }
+   }
+    public static void main(String[] args) {
+        int[] arr = {10,30,50,70};
+         Node head = convertArrToDLL(arr);
+          head = deletionHead(head);
+         print(head);
+    }
+}
